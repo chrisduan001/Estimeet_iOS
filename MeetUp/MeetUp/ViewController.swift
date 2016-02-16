8a341749f86644928d344df1d5f43b74d259a79a
chrisduan001 <AmyDuan@Chriss-iMac.telecom>
2016-02-16 20:20:22 +1300

    init

---
+++ MeetUp/MeetUp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MeetUp
+//
+//  Created by Chris Duan on 16/02/16.
+//  Copyright © 2016 Chris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
