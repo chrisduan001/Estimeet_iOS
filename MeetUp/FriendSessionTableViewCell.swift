54aeb57a21fe571dc7a3b106f3e77e6b07d7d9c6
chris <chris.duan@gmail.com>
2016-05-07 22:58:52 +1200

    cell ui

---
+++ MeetUp/FriendSessionTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  FriendSessionTableViewCell.swift
+//  MeetUp
+//
+//  Created by Chris Duan on 7/05/16.
+//  Copyright © 2016 Chris. All rights reserved.
+//
+
+import UIKit
+
+class FriendSessionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
